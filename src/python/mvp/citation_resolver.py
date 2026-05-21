45fddbc3339f21515e93303555b96570d78a25dc
Cliff Wulfman <cwulfman@princeton.edu>
2026-05-21 18:38:45 -0400

    First commit of citation_resolver.py
    This code is being ported from Charles Pletcher's
    module.

---
+++ src/python/mvp/citation_resolver.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+from lxml import etree
+
+
+class CitationResolver:
+    def __init__(self) -> None:
+        pass
+
+    
+    def resolve(self, citation:str) -> str:
+        pass
